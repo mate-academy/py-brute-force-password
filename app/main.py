e0bd07a7e64285ce8c9a340a46b7568c78a285b6
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-12-03 08:55:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+import itertools
+# import multiprocessing
 import time
-from hashlib import sha256
+from concurrent.futures import ProcessPoolExecutor
 
+from hashlib import sha256
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -20,13 +23,36 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    pass
+def brute_force_password(password) -> None:
+    for pwd in itertools.product("01233456789", repeat=8):
+        pwd = "".join(pwd)
+        if sha256_hash_str(pwd) == password:
+            print(f"Password found: {pwd}")
+            break
+
+
+# def main():
+#     tasks = []
+#
+#     for pwd in PASSWORDS_TO_BRUTE_FORCE:
+#         tasks.append(multiprocessing.Process(
+#             target=brute_force_password,
+#             args=(pwd,)
+#         ))
+#         tasks[-1].start()
+#
+#     for task in tasks:
+#         task.join()
+
+
+def main():
+    with ProcessPoolExecutor() as executor:
+        executor.map(brute_force_password, PASSWORDS_TO_BRUTE_FORCE)
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    main()
     end_time = time.perf_counter()
 
     print("Elapsed:", end_time - start_time)
