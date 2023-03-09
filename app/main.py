caeddd6f05e2e196719c3078e721c444f918a3e5
AlenOl <stoveprofi@gmail.com>
2023-03-09 15:29:27 -0800

    Implemented brute_force_password

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import multiprocessing
 import time
+from concurrent.futures import ProcessPoolExecutor, wait
 from hashlib import sha256
-
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -15,18 +16,33 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
 ]
 
+SIZE = 100000000 // (multiprocessing.cpu_count() - 1)
+
 
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
+def get_password(start_password: int, end_password: int) -> None:
+    for password in range(start_password, end_password + 1):
+        if len(str(password)) < 8:
+            password = str(password).rjust(8, "0")
+        if sha256_hash_str(str(password)) in PASSWORDS_TO_BRUTE_FORCE:
+            print(f'Password: {password}')
+
+
 def brute_force_password() -> None:
-    pass
+    futures = []
+    with ProcessPoolExecutor(multiprocessing.cpu_count() - 1) as executor:
+        for i in range(multiprocessing.cpu_count() - 1):
+            futures.append(executor.submit(
+                get_password, i * SIZE, (i + 1) * SIZE)
+            )
+    wait(futures)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     start_time = time.perf_counter()
     brute_force_password()
     end_time = time.perf_counter()
-
     print("Elapsed:", end_time - start_time)
