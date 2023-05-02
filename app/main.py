4f6c0908f0f71d6cbc686d39eefdc6a7c67a6cad
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-05-02 20:16:57 +0300

    Inmplement brute force password function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+import itertools
+import multiprocessing
 import time
+from concurrent.futures import ProcessPoolExecutor
 from hashlib import sha256
-
+import random
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -20,13 +23,26 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    pass
+def brute_force_password(some_hash_password: str) -> None:
+    for password in itertools.product("0123456789", repeat=8):
+
+        password = "".join(password)
+        hash_password = sha256_hash_str(password)
+
+        if hash_password == some_hash_password:
+            print(f"{password} corresponds to {hash_password}")
+            break
+
+
+def process_pool_executor():
+
+    with ProcessPoolExecutor(multiprocessing.cpu_count() - 1) as executor:
+        executor.map(brute_force_password, PASSWORDS_TO_BRUTE_FORCE)
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    process_pool_executor()
     end_time = time.perf_counter()
 
     print("Elapsed:", end_time - start_time)
