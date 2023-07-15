3153a96f4efd8d00c81f86010f9da0a375442841
OleksandrYanchuk <sashajanchuck13@gmail.com>
2023-07-15 19:24:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import time
+import concurrent.futures
+import multiprocessing
 from hashlib import sha256
 
-
-PASSWORDS_TO_BRUTE_FORCE = [
+PASSWORDS_TO_BRUTE_FORCE = {
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
     "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
     "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
@@ -13,20 +14,44 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
     "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
-]
+}
 
 
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    pass
+def find_password_range(start: int, end: int):
+    found_passwords = set()
+    for num in range(start, end):
+        password_to_try = "{:08}".format(num)
+        hashed_password = sha256_hash_str(password_to_try)
+        if hashed_password in PASSWORDS_TO_BRUTE_FORCE:
+            found_passwords.add(password_to_try)
+    return found_passwords
+
+
+def brute_force_password():
+    max_processes = multiprocessing.cpu_count() - 1
+    big_num = 10 ** 7
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_processes) as executor:
+        futures = [
+            executor.submit(find_password_range, i * big_num, (i + 1) * big_num)
+            for i in range(len(PASSWORDS_TO_BRUTE_FORCE))
+        ]
+
+    all_found_passwords = set()
+    for future in concurrent.futures.as_completed(futures):
+        all_found_passwords.update(future.result())
+
+    return all_found_passwords
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    found_passwords = brute_force_password()
     end_time = time.perf_counter()
 
     print("Elapsed:", end_time - start_time)
+    print("Found passwords:", found_passwords)
