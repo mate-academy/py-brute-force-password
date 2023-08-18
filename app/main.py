06924b743d2f877984e262fa603665eae6e31456
Mariia Horbova <gorbova21@gmail.com>
2023-08-18 22:02:10 +0300

    implemented brute-force

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import time
 from hashlib import sha256
 
@@ -20,8 +21,30 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
+def check_password(start: int, end: int) -> None:
+    for password in range(start, end):
+        password_str = str(password).zfill(8)
+        hashed_password = sha256_hash_str(password_str)
+
+        if hashed_password in PASSWORDS_TO_BRUTE_FORCE:
+            print(f"Password found: {password_str} -> Hash: {hashed_password}")
+
+
 def brute_force_password() -> None:
-    pass
+    processes = multiprocessing.cpu_count()
+    chunk_size = 10_000_000
+
+    tasks = []
+
+    for i in range(processes):
+        tasks.append(multiprocessing.Process(
+            target=check_password,
+            args=(i * chunk_size, (i + 1) * chunk_size)
+        ))
+        tasks[-1].start()
+
+    for task in tasks:
+        task.join()
 
 
 if __name__ == "__main__":
