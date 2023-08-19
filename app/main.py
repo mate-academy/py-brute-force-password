2cb05b3477ea643af5819af9f119ee10b8ed3ae5
Anton Shpakovych <shpakovych18@gmail.com>
2023-08-19 17:38:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import time
 from hashlib import sha256
 
@@ -20,8 +21,30 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
+def validate_password(start: int, end: int) -> None:
+    for password in range(start, end):
+        password_str = str(password).zfill(8)
+        sha256_password = sha256_hash_str(password_str)
+
+        if sha256_password in PASSWORDS_TO_BRUTE_FORCE:
+            print(f"PASSWORD-{password} HASH-{sha256_password}")
+
+
 def brute_force_password() -> None:
-    pass
+    processes = multiprocessing.cpu_count()
+    chunk_size = 10_000_000
+
+    tasks = []
+
+    for i in range(processes):
+        tasks.append(multiprocessing.Process(
+            target=validate_password,
+            args=(i * chunk_size, (i + 1) * chunk_size)
+        ))
+        tasks[-1].start()
+
+    for task in tasks:
+        task.join()
 
 
 if __name__ == "__main__":
