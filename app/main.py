3e82260aaa1c36df3f8b963d400c4d2d9c6031d4
Oleh Fadieiev <61756426+drivemax26@users.noreply.github.com>
2023-10-03 20:03:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import time
 from hashlib import sha256
-
+import itertools
+from multiprocessing import Pool, cpu_count
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -20,13 +21,33 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    pass
+def brute_force_password(password):
+    hashed_password = sha256_hash_str(password)
+    if hashed_password in PASSWORDS_TO_BRUTE_FORCE:
+        return password
+
+
+def main():
+    charset = "0123456789"
+    password_length = 8
+
+    max_processes = cpu_count()
+
+    possible_passwords = ["".join(password) for password in itertools.product(charset, repeat=password_length)]
+
+    with Pool(max_processes) as pool:
+        results = pool.map(brute_force_password, possible_passwords)
+
+    for result in results:
+        if result:
+            print(f"Correct password: {result}")
+
+    print("Done!")
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    main()
     end_time = time.perf_counter()
 
     print("Elapsed:", end_time - start_time)
