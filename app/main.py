ba52bf3e7a615859e82d19c6c12df4cf0478e3c2
OlegatorLE <oleh.travin@gmail.com>
2023-10-29 10:40:09 +0200

    Added find_password function & multiprocessing logic to find passwords from hash

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import time
+from concurrent.futures import ProcessPoolExecutor, wait
 from hashlib import sha256
-
+from multiprocessing import cpu_count
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -20,8 +21,31 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
+def find_password(start: int, end: int) -> None:
+    for password in range(start, end):
+        eight_num_password = str(password).zfill(8)
+        hashed_password = sha256_hash_str(eight_num_password)
+        if hashed_password in PASSWORDS_TO_BRUTE_FORCE:
+            print(
+                f"{PASSWORDS_TO_BRUTE_FORCE.index(hashed_password) + 1} "
+                f"Password: {eight_num_password}\n"
+                f"Hash: {hashed_password}\n {'-' * 70}"
+            )
+
+
 def brute_force_password() -> None:
-    pass
+    futures = []
+
+    with ProcessPoolExecutor(cpu_count() - 1) as executor:
+        for num in range(len(PASSWORDS_TO_BRUTE_FORCE)):
+            futures.append(
+                executor.submit(
+                    find_password,
+                    num * 10**7,
+                    num * 10**7 + 10**7
+                )
+            )
+        wait(futures)
 
 
 if __name__ == "__main__":
