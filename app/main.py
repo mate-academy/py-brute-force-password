a2b32d268c201154715c7010e20a6afb8856faaf
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-10-30 20:19:58 +0200

    Implemented multiprocess_executor

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+from concurrent.futures import ProcessPoolExecutor, wait
+import multiprocessing
 import time
 from hashlib import sha256
-
+from itertools import product
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -20,13 +22,26 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    pass
+def brute_force_password(start: int, end: int):
+    for raw_password in range(start, end):
+        password = f"{raw_password:0>8}"
+        hashed_password = sha256_hash_str(password)
+        if hashed_password in PASSWORDS_TO_BRUTE_FORCE:
+            print(f"{password} hashed as {hashed_password}")
+
+
+def main_multiprocess_executor():
+    results = []
+    with ProcessPoolExecutor(multiprocessing.cpu_count() - 1) as executor:
+        gap = 10 ** 7
+        for step in range(10):
+            results.append(executor.submit(brute_force_password, step * gap, (step + 1) * gap))
+    wait(results)
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    main_multiprocess_executor()
     end_time = time.perf_counter()
 
     print("Elapsed:", end_time - start_time)
