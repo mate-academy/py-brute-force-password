f4f2c3569d2f97e9ffa8a89d39fed2527d845fd9
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-11-01 18:25:05 +0200

    Solution4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import multiprocessing
 import time
+from concurrent.futures import ProcessPoolExecutor, wait
 from hashlib import sha256
-from itertools import product
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -23,8 +24,10 @@ def sha256_hash_str(to_hash: str) -> str:
 def brute_force_password() -> None:
     counter = 1
 
-    for password_candidate in product("0123456789", repeat=8):
-        password = "".join(password_candidate)
+    for password_candidate in range(10**8):
+        password = str(password_candidate)
+        if len(password) < 8:
+            password = "0" * (8 - len(password)) + password
         hash_password = sha256_hash_str(password)
         if hash_password in PASSWORDS_TO_BRUTE_FORCE:
             print(f"{counter}): {password}")
@@ -34,9 +37,18 @@ def brute_force_password() -> None:
             break
 
 
+def main_mult() -> None:
+
+    futures = []
+
+    with ProcessPoolExecutor(multiprocessing.cpu_count() - 1) as executor:
+        futures.append(executor.submit(brute_force_password))
+
+    wait(futures)
+
+
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    main_mult()
     end_time = time.perf_counter()
-
     print("Elapsed:", end_time - start_time)
