3126d0bc4c6e3be0584998a75bc716e5b8821da1
ladno338 <pda130507@gmail.com>
2024-04-11 12:58:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -19,7 +19,8 @@ PASSWORDS_TO_BRUTE_FORCE = [
 
 def to_pass_format(number: int) -> str:
     str_number = str(number)
-    return "0" * (8 - len(str_number)) + str_number
+    # return "0" * (8 - len(str_number)) + str_number
+    return str_number.zfill(8 - len(str_number)) + str_number
 
 
 def sha256_hash_str(to_hash: str) -> str:
