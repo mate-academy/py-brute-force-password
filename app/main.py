54e5c33d1a40c7ff560cf768f1448fc3c22db3e3
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-11-01 08:45:24 +0100

    add search_password function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+import multiprocessing
 import time
+from concurrent.futures import ProcessPoolExecutor, wait
 from hashlib import sha256
-
+import itertools
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -16,17 +18,39 @@ PASSWORDS_TO_BRUTE_FORCE = [
 ]
 
 
+def search_passwords():
+    # dict = {}
+    passwords = []
+    combinations = itertools.product('0123456789', repeat=8)
+    combinations_list = [''.join(combination) for combination in combinations]
+    for combo in combinations_list:
+        dict = {combo: sha256(combo.encode("utf-8")).hexdigest()}
+        for key, value in dict.items():
+            if value in PASSWORDS_TO_BRUTE_FORCE:
+                passwords.append(key)
+                print(passwords)
+
+
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    pass
+def print_passwords(passwords):
+    print(passwords)
+
+
+def brute_force_password(passwords) -> None:
+    future = []
+    with ProcessPoolExecutor(multiprocessing.cpu_count() - 1) as executor:
+        for password in passwords:
+            future.append(executor.submit(print_passwords, password))
+    wait(future)
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
     brute_force_password()
+    search_passwords()
     end_time = time.perf_counter()
 
     print("Elapsed:", end_time - start_time)
