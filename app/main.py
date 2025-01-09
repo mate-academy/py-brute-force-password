df7264f1b99282e0d8b0f14d180903f9a0b290d2
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2025-01-09 03:49:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import time
 from hashlib import sha256
-
+from itertools import product
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -15,18 +15,22 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
 ]
 
-
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
-
 def brute_force_password() -> None:
-    pass
-
+    found_passwords = []
+    for digits in product('0123456789', repeat=8):
+        password = ''.join(digits)
+        hashed_password = sha256_hash_str(password)
+        if hashed_password in PASSWORDS_TO_BRUTE_FORCE:
+            found_passwords.append(password)
+            print(password)
+            if len(found_passwords) == len(PASSWORDS_TO_BRUTE_FORCE):
+                break
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
     brute_force_password()
     end_time = time.perf_counter()
-
     print("Elapsed:", end_time - start_time)
