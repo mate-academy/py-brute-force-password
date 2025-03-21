9a9458bee0e3256d37ad53ff8d7e0bd5773b2717
halyna-baklanova <novabalka@gmail.com>
2025-03-21 18:08:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor, wait
 import time
 from hashlib import sha256
 
-
-PASSWORDS_TO_BRUTE_FORCE = [
+PASSWORD_HASHES_SET = {
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
     "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
     "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
@@ -13,15 +14,27 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
     "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
-]
+}
 
 
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
+def find_password(hash_password: str):
+    for i in range(10 ** 8):
+        password = f"{i:08d}"
+        hashed_password = sha256_hash_str(password)
+        if hashed_password == hash_password:
+            print(f"{password} equals to {hashed_password}")
 
-def brute_force_password() -> None:
-    pass
+def brute_force_password():
+    tasks = []
+
+    with ProcessPoolExecutor(multiprocessing.cpu_count() - 1) as executor:
+        for hash_password in PASSWORD_HASHES_SET:
+            tasks.append(executor.submit(find_password, hash_password))
+
+    wait(tasks)
 
 
 if __name__ == "__main__":
